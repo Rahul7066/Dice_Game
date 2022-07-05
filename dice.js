3d8be0babe0f4520ceb6de7d689c2f03a5b42d07
Rahul Sonawane <86938802+Rahul7066@users.noreply.github.com>
2022-07-05 09:24:32 +0530

    files fo dice game

---
+++ dice.js
@@ -0,0 +1,32 @@
+// play button
+
+function refreshPage(){
+    window.location.reload();
+} 
+// selecting the first dice
+
+var randomNumber1 = Math.floor(Math.random() *6) + 1; //1-6
+var randomDiceImage = "dice" + randomNumber1 +".png"; //dice1.png to dice6.png
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png to images/dice6.png
+var image1 = document.querySelectorAll('img')[0];
+image1.setAttribute("src",randomImageSource);
+
+// selecting the second dice
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var randomDiceImage = "dice" + randomNumber2 +".png";
+var randomImageSource = "images/" + randomDiceImage;
+var image2 = document.querySelectorAll('img')[1];
+image2.setAttribute("src", randomImageSource);
+
+//h1 Changing
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins 🚩"
+}
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins🚩"
+}
+else{
+    document.querySelector("h1").innerHTML = "Play Again"
+}
